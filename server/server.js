dfe7b2dac4e70f9c37493bc75cb641a558e7afe7
Albert Zhang <albertzhang@gmail.com>
2021-01-16 13:02:21 -0800

    cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 app.use(express.static("public"));
+app.use(cors());
 // rooms => 
 // {room_id: {
 //     users: [Users], 
