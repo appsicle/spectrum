3282f8c2c45faa3331e97d0a971a4a783183953e
justinekoa <justinekoa@gmail.com>
2021-01-16 03:23:08 -0800

    im tired

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ io.on("connection", (socket) => {
         }
       };
     }
-    clients[roomId][users].push(user);
+    clients[roomId].users.push(user);
 
     socket.join(roomId);
     io.to(roomId).emit("user-connected", user);
@@ -42,7 +42,7 @@ io.on("connection", (socket) => {
       console.log('disconnect');
       io.to(roomId).emit("user-disconnected", user);
       // remove userId to list of connected clients
-      clients[roomId][users] = clients[roomId][users].filter((obj) => obj.id !== user.id);
+      clients[roomId].users = clients[roomId].users.filter((obj) => obj.id !== user.id);
     });
   });
 
@@ -53,40 +53,40 @@ io.on("connection", (socket) => {
   // start emits to all connected sockets a round
   socket.on("start", (roomId) => {
     // all users are available to be the first speaker
-    clients[roomId][round][unusedSpeakers] = clients[roomId][users]; 
-    clients[roomId][round][prompt] = "Temporary Prompt"; // todo make this select a random prompt that is NOT in finishedPrompts
+    clients[roomId].round.unusedSpeakers = clients[roomId].users; 
+    clients[roomId].round.prompt = "Temporary Prompt"; // todo make this select a random prompt that is NOT in finishedPrompts
     
-    const numPossibleSpeakers = clients[rootId][round][unusedSpeakers].length;
+    const numPossibleSpeakers = clients[rootId].round.unusedSpeakers.length;
     const index = Math.floor(Math.random()*numPossibleSpeakers);
     const speaker = users[index];
 
     // remove the chosen first speaker from the available speakers
-    clients[roomId][round][unusedSpeakers].splice(index, 1); 
+    clients[roomId].round.unusedSpeakers.splice(index, 1); 
 
     // set first speaker
-    clients[roomId][round][currentSpeakers] = speaker;
+    clients[roomId].round.unusedSpeakers = speaker;
 
     // emit round information 
-    io.to(roomId).emit("round-updated", clients[roomId][round]);
+    io.to(roomId).emit("round-updated", clients[roomId].round);
     // onResetTimer(roomId); // emit 30 second timer
   });
 
   socket.on("next-speaker", (roomId) => {
-    const numPossibleSpeakers = clients[rootId][round][unusedSpeakers].length;
+    const numPossibleSpeakers = clients[rootId].round.unusedSpeakers.length;
     const index = Math.floor(Math.random()*numPossibleSpeakers);
     const speaker = users[index];
 
     // remove the chosen first speaker from the available speakers
-    clients[roomId][round][unusedSpeakers].splice(index, 1); 
+    clients[roomId].round.unusedSpeakers.splice(index, 1); 
 
     // set speaker
-    clients[roomId][round][currentSpeakers] = speaker;
-    io.to(roomId).emit("round-updated", clients[roomId][round]);
+    clients[roomId].round.unusedSpeakers = speaker;
+    io.to(roomId).emit("round-updated", clients[roomId].round);
   });
 
   socket.on("next-question", (roomId) => {
-    clients[roomId][round][prompt] = "Updated Temporary Prompt"; // todo make this select a new random prompt the room hasn't seen before
-    io.to(roomId).emit("round-updated", clients[roomId][round]);
+    clients[roomId].round.prompt = "Updated Temporary Prompt"; // todo make this select a new random prompt the room hasn't seen before
+    io.to(roomId).emit("round-updated", clients[roomId].round);
   });
 
 });
