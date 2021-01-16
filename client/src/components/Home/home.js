a4f0686d1ed3f00b0f0e43b948bf713d55b5b388
Albert Zhang <albertzhang@gmail.com>
2021-01-15 22:20:18 -0800

    create home page form

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
+import './home.css';
+
 function Home() {
+  const [toCreate, setToCreate] = useState(false);
+
   return (
     <div className="home-container">
-     home
+     <div className="form-container">
+       <div className="room-action-button-container">
+         <button className="join-game-button" onClick={() => {
+           setToCreate(false);
+         }}>
+           Join Game
+         </button>
+         <button className="create-game-button" onClick={() => {
+           setToCreate(true);
+         }}>
+           Create Game
+         </button>
+       </div>
+       <div className="input-container">
+         <input type="text" className="name-input" placeholder="Nickname" />
+         <input type="text" className="room-input" placeholder="Room Name" />
+         <button className="submit-button">
+           {toCreate ? 'Create room' : 'Join room'}
+         </button>
+       </div>
+     </div>
     </div>
   );
 }
